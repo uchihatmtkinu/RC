2a8e3b03f69c522cc3fabb6f7f871359384e8da6
uchihatmtkinu <uchihatmtkinu@gmail.com>
2018-06-14 17:30:57 +0800

    add

--- testforclient/network/client_variable.go
+++ testforclient/network/client_variable.go
@@ -1,53 +1,53 @@
 package network
 
 import (
-	"github.com/uchihatmtkinu/RC/shard"
-	"github.com/uchihatmtkinu/RC/account"
-	"github.com/uchihatmtkinu/RC/Reputation/cosi"
-	"github.com/uchihatmtkinu/RC/ed25519"
 	"github.com/uchihatmtkinu/RC/Reputation"
+	"github.com/uchihatmtkinu/RC/Reputation/cosi"
+	"github.com/uchihatmtkinu/RC/account"
+	"github.com/uchihatmtkinu/RC/ed25519"
+	"github.com/uchihatmtkinu/RC/shard"
 )
 
 const protocol = "tcp"
 const nodeVersion = 1
 const commandLength = 12
 const bufferSize = 1000
-const timeoutCosi = 10  //10seconds for timeout
+const timeoutCosi = 10 //10seconds for timeout
 
-var MyAccount 		account.RcAcc
+var MyAccount account.RcAcc
 
-var leaderAddr 		string
-var addrMapToInd 	map[string]int //ip+port
-var groupMems		[]shard.MemShard
-var numMems			int
+var leaderAddr string
+var addrMapToInd map[string]int //ip+port
+var groupMems []shard.MemShard
+var numMems int
 
 //used in pow
 var MyPoW Reputation.ProofOfWork
 
 //used in commitCh
 type commitInfoCh struct {
-	addr	string
+	addr    string
 	request []byte
 }
 
 type challengeMessage struct {
-	aggregatePublicKey	ed25519.PublicKey
-	aggregateCommit		cosi.Commitment
+	aggregatePublicKey ed25519.PublicKey
+	aggregateCommit    cosi.Commitment
 }
 
 type responseInfoCh struct {
-	addr		string
-	request 	[]byte
+	addr    string
+	request []byte
 }
 
 type cosiSigMessage struct {
-	pubKeys	[]ed25519.PublicKey
-	cosiSig		cosi.SignaturePart
+	pubKeys []ed25519.PublicKey
+	cosiSig cosi.SignaturePart
 }
 
 //channel
-var cosiAnnounceCh	chan []byte
-var cosiCommitCh 	chan commitInfoCh
-var cosiChallengeCh	chan []byte
-var cosiResponseCh	chan responseInfoCh
-var cosiSigCh		chan []byte
+var cosiAnnounceCh chan []byte
+var cosiCommitCh chan commitInfoCh
+var cosiChallengeCh chan []byte
+var cosiResponseCh chan responseInfoCh
+var cosiSigCh chan []byte
