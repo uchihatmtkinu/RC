00e1e31054ac7794a4e088de91ef621ad0e4065e
uchihatmtkinu <uchihatmtkinu@gmail.com>
2018-07-06 13:34:22 +0800

    xx

--- testforclient/main.go
+++ testforclient/main.go
@@ -1,15 +1,123 @@
 package main
 
 import (
-	"github.com/uchihatmtkinu/RC/testforclient/network"
+	"encoding/json"
 	"fmt"
-	"github.com/uchihatmtkinu/RC/shard"
-	"github.com/uchihatmtkinu/RC/gVar"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/uchihatmtkinu/RC/basic"
 )
 
+type PrevOut struct {
+	Addr  string `json:"addr"`
+	Value int    `json:"value"`
+}
+
+type JsInput struct {
+	Prev PrevOut `json:"prev_out"`
+}
+
+type JsOut struct {
+	Addr  string `json:"addr"`
+	Value int    `json:"value"`
+}
+
+type JsTx struct {
+	Hash   string    `json:"hash"`
+	VinSz  int       `json:"vin_sz"`
+	VoutSz int       `json:"vout_sz"`
+	Input  []JsInput `json:"inputs"`
+	Out    []JsOut   `json:"out"`
+}
+
+type JsBlock struct {
+	Hash      string `json:"hash"`
+	Prevblock string `json:"prev_block"`
+	Fee       int    `json:"fee"`
+	Ntx       int    `json:"n_tx"`
+	Height    int    `json:"height"`
+	Tx        []JsTx `json:"tx"`
+}
+
+type JsData struct {
+	Blocks []JsBlock `json:"blocks"`
+}
+
+var txCnt int
+
+func JsonMake(stat int, start int, last int, name int) int {
+	//url := "https://blockchain.info/rawblock/00000000d1145790a8694403d4063f323d499e655c83426834d4ce2f8dd4a2ee"
+	var file *os.File
+	if stat == 0 {
+		file, _ = os.Create("TxData" + strconv.Itoa(name) + ".txt")
+	} else {
+		file, _ = os.OpenFile("TxData"+strconv.Itoa(name)+".txt", os.O_WRONLY|os.O_APPEND, 0666)
+	}
+	defer file.Close()
+	for i := start; i < last; i++ {
+		url := "https://blockchain.info/block-height/" + strconv.Itoa(i) + "?format=json"
+		spaceClient := http.Client{
+			Timeout: time.Second * 2,
+		}
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+		}
+		req.Header.Set("User-Agent", "spacecount-tutorial")
+		res, getErr := spaceClient.Do(req)
+		if getErr != nil {
+			//log.Fatal(getErr)
+			return i
+		}
+		body, readErr := ioutil.ReadAll(res.Body)
+		if readErr != nil {
+			//log.Fatal(readErr)
+			return i
+		}
+		tmp := JsData{}
+		jsonErr := json.Unmarshal(body, &tmp)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+			//return i
+		}
+		tmp2, parErr := json.Marshal(tmp)
+		if parErr != nil {
+			log.Fatal(parErr)
+		}
+
+		//fmt.Println(body)
+		file.Write(basic.ByteSlice(uint32(len(tmp2))))
+		file.Write(tmp2)
+
+		fmt.Println(i)
+	}
+
+	return last
+	/*file, _ = os.Open("TxData.txt")
+	for i := 100000; i < 100200; i++ {
+		tmp1 := make([]byte, 4)
+		file.Read(tmp1)
+		var lenX uint32
+		basic.Decode(&tmp1, &lenX)
+		tmp2 := make([]byte, lenX)
+		file.Read(tmp2)
+		tmp := JsData{}
+		jsonErr := json.Unmarshal(tmp2, &tmp)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+		}
+		fmt.Println(tmp)
+	}
+	file.Close()*/
+
+}
 
 func main() {
-	totalNum := int(gVar.ShardSize*gVar.ShardCnt)
+	/*totalNum := int(gVar.ShardSize * gVar.ShardCnt)
 	fmt.Println("test begin")
 	network.StartServer(1)
 
@@ -17,14 +125,45 @@ func main() {
 
 	network.ShardProcess()
 
-	for i:=0 ; i < totalNum; i++ {
+	for i := 0; i < totalNum; i++ {
 		fmt.Println()
 		shard.GlobalGroupMems[i].AddRep(int64(i))
 		shard.GlobalGroupMems[i].SetTotalRep(int64(i))
-		shard.GlobalGroupMems[i].SetTotalRep(int64(i+1))
-		shard.GlobalGroupMems[i].SetTotalRep(int64(i+2))
+		shard.GlobalGroupMems[i].SetTotalRep(int64(i + 1))
+		shard.GlobalGroupMems[i].SetTotalRep(int64(i + 2))
 		shard.GlobalGroupMems[i].Print()
 		fmt.Println(shard.GlobalGroupMems[i].CalTotalRep())
 	}
-	network.RepProcess(&shard.GlobalGroupMems)
+	network.RepProcess(&shard.GlobalGroupMems)*/
+	txCnt = 0
+	/*start := 150000
+	last := start + 50000
+	currentBlockID := start
+
+	for i := 0; i < last; i++ {
+
+		xx := JsonMake(i, currentBlockID, last, 4)
+		if xx >= last {
+			break
+		}
+		currentBlockID = xx
+	}*/
+	file, _ := os.Open("TxData4.txt")
+	for i := 0; i < 50000; i++ {
+		tmp1 := make([]byte, 4)
+		file.Read(tmp1)
+		var lenX uint32
+		basic.Decode(&tmp1, &lenX)
+		tmp2 := make([]byte, lenX)
+		file.Read(tmp2)
+		tmp := JsData{}
+		jsonErr := json.Unmarshal(tmp2, &tmp)
+		if jsonErr != nil {
+			log.Fatal(jsonErr)
+		}
+		fmt.Println(tmp.Blocks[0].Height)
+		txCnt += tmp.Blocks[0].Ntx
+	}
+	fmt.Println(txCnt)
+	file.Close()
 }
