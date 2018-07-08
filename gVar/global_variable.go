129b5d3c9f108e176d438cc5e3df88375c0c6c2c
Zeyu WANG <uchihatmtkinu@gmail.com>
2018-07-08 11:27:27 +0800

    123

--- gVar/global_variable.go
+++ gVar/global_variable.go
@@ -4,7 +4,7 @@ package gVar
 const MagicNumber byte = 66
 
 //ShardSize is the number of miners in one shard
-const ShardSize uint32 = 10
+const ShardSize uint32 = 40
 
 //ShardCnt is the number of shards
 const ShardCnt uint32 = 1
