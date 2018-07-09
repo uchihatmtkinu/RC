5704518f6ad6eb1146a796795cc47b8d8190204c
Zeyu WANG <uchihatmtkinu@gmail.com>
2018-07-09 22:06:25 +0800

    xxx

--- testforclient/network/TxSendProtocol.go
+++ testforclient/network/TxSendProtocol.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 
 //SendTx is the protocol for sending
 func SendTx(x *[]byte) {
+	fmt.Println(time.Since(gVar.T1), CacheDbRef.ID, "sending TxBatch")
 	for i := 0; i < int(gVar.ShardSize); i++ {
 		xx := shard.ShardToGlobal[CacheDbRef.ShardNum][i]
 		if xx != int(CacheDbRef.ID) {
