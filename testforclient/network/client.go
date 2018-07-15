5fc118f03abc8cfc6d48de1ae56cac5738b7355b
Zeyu WANG <uchihatmtkinu@gmail.com>
2018-07-15 18:15:08 +0800

    123

--- testforclient/network/client.go
+++ testforclient/network/client.go
@@ -77,7 +77,7 @@ func handleConnection(conn net.Conn, requestChannel chan []byte) {
 func StartServer(ID int) {
 
 	//ln, err := net.Listen(protocol, shard.MyMenShard.Address)
-	ln, err := net.Listen(protocol, "*")
+	ln, err := net.Listen(protocol, "0.0.0.0:0")
 	fmt.Println("My IP+Port: ", shard.MyMenShard.Address)
 	if err != nil {
 		log.Panic(err)
