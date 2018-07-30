2e9eb73f7d3321634e233684daa2179a71e44a1b
Zeyu WANG <uchihatmtkinu@gmail.com>
2018-07-30 13:35:39 +0800

    123

--- gVar/global_variable.go
+++ gVar/global_variable.go
@@ -6,10 +6,10 @@ import "time"
 const MagicNumber byte = 66
 
 //ShardSize is the number of miners in one shard
-const ShardSize uint32 = 300
+const ShardSize uint32 = 225
 
 //ShardCnt is the number of shards
-const ShardCnt uint32 = 6
+const ShardCnt uint32 = 4
 
 //used in rep calculation, scaling factor
 const RepTP = 1
