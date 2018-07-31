93b7b743cb50069db937d3d4b781aff389600230
Zeyu WANG <uchihatmtkinu@gmail.com>
2018-07-31 15:26:37 +0800

    change parameter

--- gVar/global_variable.go
+++ gVar/global_variable.go
@@ -22,7 +22,7 @@ const RepFN = 0
 const SlidingWindows = 20
 
 //NumTxListPerEpoch is the number of txblocks in one epoch
-const NumTxListPerEpoch = 7 //60
+const NumTxListPerEpoch = 10 //60
 
 //NumTxBlockForRep is the number of blocks for one rep block
 const NumTxBlockForRep = 4 //10
